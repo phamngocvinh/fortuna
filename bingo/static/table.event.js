e4326754af8f92dc8c23f9858a6a1eb8cd831bfc
Pham Ngoc Vinh <vinhgalaxys2@gmail.com>
2023-12-09 22:21:53 +0700

    fix bug highlight

--- bingo/static/table.event.js
+++ bingo/static/table.event.js
@@ -1,10 +1,25 @@
 // Get all table cells
-const cells = document.querySelectorAll('#myTable td')
+const cellsS = document.querySelectorAll('#myTableL td')
 
 // Loop through each cell and add click event listener
-cells.forEach((cell) => {
+cellsS.forEach((cell) => {
   cell.addEventListener('click', () => {
     // Toggle highlight on the clicked cell
     cell.classList.toggle('table-warning')
   })
 })
+
+const cellsL = document.querySelectorAll('#myTableS td')
+
+// Loop through each cell and add click event listener
+cellsL.forEach((cell) => {
+  cell.addEventListener('click', () => {
+    // Toggle highlight on the clicked cell
+    cell.classList.toggle('table-warning')
+  })
+})
+
+window.onload = function() {
+    document.getElementById("myTableS").rows[2].cells[2].classList.add("table-warning");
+    document.getElementById("myTableL").rows[2].cells[2].classList.add("table-warning");
+};
